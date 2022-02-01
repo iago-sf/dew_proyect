0abbac7c2d92f024fbdce6c8ec89fb5d2278f1cd
iago <iaguerreles@gmail.com>
2022-02-01 22:48:55 +0000

    Router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <nav>
+      <router-link to="/">Home</router-link>&nbsp;
+      <router-link to="/about">Acerca de</router-link>
+  </nav>
+  <h1>{{ name }}</h1>
+  <button @click="changeName">click</button>
+  <router-view />
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const name = ref('Manu');
+
+function changeName() {
+  name.value = 'iago';
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
