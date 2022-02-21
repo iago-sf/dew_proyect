13edd0569464d175ca7bf809829985c2808e95b8
iago <iaguerreles@gmail.com>
2022-02-21 17:42:24 +0000

    icons

--- src/store/storeApiUrl.js
+++ src/store/storeApiUrl.js
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
 export const storeApiUrl = defineStore('url', () => {
-    const url = ref("http://localhost:3001");
+    const url = ref("https://dpl-proyect.herokuapp.com");
     
     const getUrl = () => url.value;
 
